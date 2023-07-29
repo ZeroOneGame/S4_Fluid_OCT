ffca9302ffca9e4a459fcbd762ae1db353a8cc68
ZeroOneGame <75431975+ZeroOneGame@users.noreply.github.com>
2023-07-29 15:06:29 +0800

    Update data_utils.py

---
+++ data/data_utils.py
@@ -0,0 +1,46 @@
+# encoding: utf-8
+"""
+
+More details are coming soon.
+
+"""
+
+# Network Training
+
+def get_current_consistency_weight(args, epoch):
+    # Consistency ramp-up from https://arxiv.org/abs/1610.02242
+    return args.consistency * ramps.sigmoid_rampup(epoch, args.consistency_rampup)
+
+
+def update_ema_variables(model, ema_model, alpha, global_step):
+    # Use the true average until the exponential average is more correct
+    alpha = min(1 - 1 / (global_step + 1), alpha)
+    for ema_param, param in zip(ema_model.parameters(), model.parameters()):
+        ema_param.data.mul_(alpha).add_(1 - alpha, param.data)
+
+
+def create_model(args, ema, num_classes, use_ddpp2:bool=False):
+    # Network definition
+    model = net_factory(net_type=args.model, in_chns=3, class_num=num_classes, use_ddpp2=use_ddpp2, args=args)
+    if ema:
+        for param in model.parameters():
+            param.detach_()
+    return model
+
+def kaiming_normal_init_weight(model):
+    for m in model.modules():
+        if isinstance(m, nn.Conv2d):
+            torch.nn.init.kaiming_normal_(m.weight)
+        elif isinstance(m, nn.BatchNorm2d):
+            m.weight.data.fill_(1)
+            m.bias.data.zero_()
+    return model
+
+def xavier_normal_init_weight(model):
+    for m in model.modules():
+        if isinstance(m, nn.Conv2d):
+            torch.nn.init.xavier_normal_(m.weight)
+        elif isinstance(m, nn.BatchNorm2d):
+            m.weight.data.fill_(1)
+            m.bias.data.zero_()
+    return model
